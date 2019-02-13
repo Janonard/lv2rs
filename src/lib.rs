9b69c463139b480972cff33898df4fcc633a99cd
Janonard <janonard@protonmail.com>
2019-02-13 17:15:30 +0100

    Trying to adapt the new lv2-core crate

--- src/lib.rs
+++ src/lib.rs
@@ -1,43 +1,30 @@
-extern crate libc;
-extern crate lv2_raw as lv2;
+extern crate lv2_core;
+use std::ffi::CStr;
 
-pub mod buffer;
-mod main_macro;
-mod plugin;
-
-use std::collections::VecDeque;
-pub use plugin::Plugin;
+use lv2_core::*;
 
 struct ExAmp {
-    delay: buffer::InputParameter,
-    input: buffer::InputBuffer,
-    output: buffer::OutputBuffer,
-
-    frate: f32,
-    backlog: VecDeque<f32>,
+    gain: ports::ParameterInputPort,
+    input: ports::AudioInputPort,
+    output: ports::AudioOutputPort,
 }
 
-impl plugin::Plugin for ExAmp {
+impl lv2_core::Plugin for ExAmp {
     fn instantiate(
-        _descriptor: *const lv2::LV2Descriptor,
-        rate: f64,
-        _bundle_path: *const i8,
-        _features: *const *const lv2::LV2Feature,
+        _rate: f64,
+        _bundle_path: &CStr,
+        _features: *const *const raw::LV2Feature,
     ) -> Self {
-        
         Self {
-            delay: buffer::InputParameter::new(),
-            input: buffer::InputBuffer::new(),
-            output: buffer::OutputBuffer::new(),
-
-            frate: rate as f32,
-            backlog: VecDeque::with_capacity(rate as usize),
+            gain: ports::ParameterInputPort::new(),
+            input: ports::AudioInputPort::new(),
+            output: ports::AudioOutputPort::new(),
         }
     }
 
     fn connect_port(&mut self, port: u32, data: *mut ()) {
         match port {
-            0 => self.delay.connect(data),
+            0 => self.gain.connect(data),
             1 => self.input.connect(data),
             2 => self.output.connect(data),
             _ => (),
@@ -47,23 +34,12 @@ impl plugin::Plugin for ExAmp {
     fn run(&mut self, n_samples: u32) {
         let input = self.input.iter(n_samples).unwrap();
         let output = self.output.iter_mut(n_samples).unwrap();
-
-        let delay = self.delay.get().unwrap();
-        let delay = (delay / 1000.0 * self.frate) as usize;
-
-        while self.backlog.len() > delay {
-            self.backlog.pop_back();
-        }
+        let gain = self.gain.get().unwrap();
 
         for (input, output) in input.zip(output) {
-            if self.backlog.len() >= delay {
-                *output = input + self.backlog.pop_back().unwrap();
-            } else {
-                *output = *input;
-            }
-            self.backlog.push_front(*input);
+            *output = input * gain;
         }
     }
 }
 
-lv2_main!(ExAmp, "https://github.com/Janonard/ExAmp");
+lv2_main!(ExAmp, b"https://github.com/Janonard/ExAmp");
