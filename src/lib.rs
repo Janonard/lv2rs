d01c4bebb8fdf37e1c65f449d7b77e6dd3dba11b
Janonard <janonard@protonmail.com>
2019-02-16 16:25:14 +0100

    Another iteration on urid

--- src/lib.rs
+++ src/lib.rs
@@ -6,46 +6,37 @@ mod raw;
 
 pub use raw::URID;
 
-pub struct Map {
-    raw: &'static raw::Map,
+pub struct Map<'a> {
+    raw: &'a mut raw::Map,
 }
 
-impl Map {
-    pub fn try_from_feature(feature: &core::feature::Feature) -> Result<Self, ()> {
-        let feature_uri = feature.get_uri()?;
-        if *(feature_uri.to_bytes()) == *(uris::MAP_URI) {
-            match unsafe { (feature.get_data() as *const raw::Map).as_ref() } {
-                Some(map) => Ok(Self { raw: map }),
-                None => Err(()),
-            }
-        } else {
-            Err(())
+impl<'a> Map<'a> {
+    pub fn try_from_feature(feature: &'a mut core::Feature) -> Option<Self> {
+        match unsafe { feature.get_data_if_uri_matches(uris::MAP_URI) } {
+            Some(map) => Some(Self { raw: map }),
+            None => return None,
         }
     }
 
-    pub fn map(&self, uri: &std::ffi::CStr) -> URID {
+    pub fn map(&mut self, uri: &std::ffi::CStr) -> URID {
         (self.raw.map)(self.raw.handle, uri.as_ptr())
     }
 }
 
-pub struct Unmap {
-    raw: &'static raw::Unmap,
+pub struct Unmap<'a> {
+    raw: &'a mut raw::Unmap,
 }
 
-impl Unmap {
-    pub fn try_from_feature(feature: &core::feature::Feature) -> Result<Self, ()> {
-        let feature_uri = feature.get_uri()?;
-        if *(feature_uri.to_bytes()) == *(uris::UNMAP_URI) {
-            match unsafe { (feature.get_data() as *const raw::Unmap).as_ref() } {
-                Some(unmap) => Ok(Self { raw: unmap }),
-                None => Err(()),
-            }
-        } else {
-            Err(())
+impl<'a> Unmap<'a> {
+    pub fn try_from_feature(feature: &'a mut core::Feature) -> Option<Self> {
+        match unsafe { feature.get_data_if_uri_matches(uris::MAP_URI) } {
+            Some(unmap) => Some(Self { raw: unmap }),
+            None => return None,
         }
     }
 
-    pub fn unmap(&self, urid: URID) -> &std::ffi::CStr {
-        unsafe { std::ffi::CStr::from_ptr((self.raw.unmap)(self.raw.handle, urid)) }
+    pub fn unmap(&mut self, urid: URID) -> &std::ffi::CStr {
+        let uri = (self.raw.unmap)(self.raw.handle, urid);
+        unsafe { std::ffi::CStr::from_ptr(uri) }
     }
 }
