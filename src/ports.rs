33f7381b9b4ebf420af8aecb7583c2aee62ac008
Janonard <janonard@protonmail.com>
2019-03-12 19:27:33 +0100

    Implementing the AtomOutputPort for scalar atoms

--- src/ports.rs
+++ src/ports.rs
@@ -1,6 +1,8 @@
-use crate::atom::{AtomHeader, AtomBody};
-use std::ptr::null_mut;
+use crate::atom::*;
+use crate::uris::MappedURIDs;
+use crate::writer::RawWriter;
 use std::marker::PhantomData;
+use std::ptr::null_mut;
 
 pub struct AtomOutputPort<A: AtomBody + Clone + ?Sized> {
     atom: *mut AtomHeader,
@@ -17,5 +19,30 @@ impl<A: AtomBody + Clone + ?Sized> AtomOutputPort<A> {
 
     pub fn connect_port(&mut self, atom: *mut AtomHeader) {
         self.atom = atom;
+    }
+
+    fn get_writer<'a>(&'a mut self) -> Result<RawWriter<'a>, ()> {
+        let data_size: usize = match unsafe { self.atom.as_ref() } {
+            Some(header) => header.size as usize,
+            None => return Err(()),
+        };
+        let data = unsafe {
+            std::slice::from_raw_parts_mut(
+                self.atom as *mut u8,
+                data_size + std::mem::size_of::<AtomHeader>(),
+            )
+        };
+        Ok(RawWriter::new(data))
+    }
+}
+
+impl<A: AtomBody + Clone + ?Sized> AtomOutputPort<A>
+where
+    A: ScalarAtomBody,
+{
+    pub fn write_atom<'a>(&'a mut self, value: &A, urid: &MappedURIDs) -> Result<&'a mut A, ()> {
+        let mut writer = self.get_writer()?;
+        writer.push_atom_header::<A>(urid)?;
+        A::construct_body(&mut writer, value)
     }
 }
