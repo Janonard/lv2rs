25332e4e17338ba1dc67b4792f3ccbab70933ce1
Janonard <janonard@protonmail.com>
2019-03-24 10:55:51 +0100

    Documenting the scalar module

--- src/scalar.rs
+++ src/scalar.rs
@@ -1,9 +1,74 @@
+//! Scalar (number-like) atoms
+//!
+//! There are several scalar atoms:
+//! * `i32`
+//! * `i64`
+//! * `f32`
+//! * `f64`
+//! * `bool`
+//! * `URID`
+//!
+//! They all have in common that they are statically sized (which is something special among atoms)
+//! and that they can be written in one piece; Once they are initialized, they are completed and
+//! need no further amendments. Therefore, their behaviour is abstracted to another trait,
+//! [`ScalarAtomBody`](trait.ScalarAtomBody.html), which features a standard implementation of
+//! [`AtomBody`](../atom/trait.AtomBody.html) for every type that implements it. Therefore, writing
+//! and reading scalar atoms is pretty straight foreward:
+//!
+//!     extern crate lv2rs_atom;
+//!     use lv2rs_atom::prelude::*;
+//!     use lv2rs_atom::uris::MappedURIDs;
+//!     use lv2rs_atom::ports::*;
+//!
+//!     pub struct Plugin {
+//!         in_port: AtomInputPort<f32>,
+//!         out_port: AtomOutputPort<f32>,
+//!         urids: &'static MappedURIDs,
+//!     }
+//!
+//!     impl Plugin {
+//!         /// Simulated `run` method.
+//!         fn run(&mut self) {
+//!             // Writing.
+//!             self.out_port.write_atom(&42.0f32, self.urids).unwrap();
+//!
+//!             // Reading.
+//!             let atom = self.in_port.get_atom(self.urids).unwrap();
+//!             assert_eq!(4, atom.body_size());
+//!             assert_eq!(self.urids.float, atom.body_type());
+//!             assert_eq!(42.0, **atom);
+//!         }
+//!     }
+//!
+//!     // Getting the default URID map.
+//!     let urids = unsafe {MappedURIDs::get_map()};
+//!
+//!     // Creating the plugin.
+//!     let mut plugin = Plugin {
+//!         in_port: AtomInputPort::new(urids),
+//!         out_port: AtomOutputPort::new(),
+//!         urids: urids,
+//!     };
+//!
+//!     // Creating the atom space.
+//!     let mut atom_space = vec![0u8; 256];
+//!     let atom = unsafe { (atom_space.as_mut_ptr() as *mut AtomHeader).as_mut() }.unwrap();
+//!     atom.size = 256 - 8;
+//!
+//!     // Connecting the ports.
+//!     plugin.in_port.connect_port(atom as &AtomHeader);
+//!     plugin.out_port.connect_port(atom);
+//!
+//!     // Calling `run`.
+//!     plugin.run();
 use crate::atom::{Atom, AtomBody, AtomHeader};
 use crate::frame::{WritingFrame, WritingFrameExt};
 use crate::uris;
 use std::ffi::CStr;
-pub use std::os::raw::c_int;
 
+/// Abstraction over scalar (number-like) atoms.
+///
+/// See the [module documentation](index.html) for more information.
 pub trait ScalarAtomBody {
     fn get_uri() -> &'static CStr;
     fn get_urid(urids: &uris::MappedURIDs) -> URID;
@@ -48,6 +113,8 @@ where
         }
     }
 }
+
+pub use std::os::raw::c_int;
 
 impl ScalarAtomBody for c_int {
     fn get_uri() -> &'static CStr {
