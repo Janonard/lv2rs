5609cd232dc72d11a2f2c96f48b6e57ab3d7da7b
Janonard <janonard@protonmail.com>
2019-03-31 14:25:44 +0200

    Expanding the midigate

--- src/lib.rs
+++ src/lib.rs
@@ -2,25 +2,40 @@ extern crate lv2rs as lv2;
 
 use lv2::atom::ports::AtomInputPort;
 use lv2::atom::prelude::*;
+use lv2::atom::sequence::TimeStamp;
 use lv2::core::{self, ports::*, *};
+use lv2::midi::atom::RawMidiMessage;
+use lv2::midi::message::StandardMidiMessage;
 use lv2::urid::*;
 use std::ffi::CStr;
 
 pub struct Midigate {
     control_port: AtomInputPort<Sequence>,
     in_port: AudioInputPort,
+    null: Vec<f32>,
     out_port: AudioOutputPort,
 
     urid_map: CachedMap,
 
-    n_active_notes: u32,
-    program: u32,
+    n_active_notes: i32,
+}
+
+impl Midigate {
+    fn assure_null_len(&mut self, min_len: usize) {
+        if self.null.len() < min_len {
+            let n_new_frames = self.null.len() - min_len;
+            self.null.reserve(n_new_frames);
+            for _ in 0..n_new_frames {
+                self.null.push(0.0);
+            }
+        }
+    }
 }
 
 impl Plugin for Midigate {
     fn instantiate(
         _descriptor: &Descriptor,
-        _rate: f64,
+        rate: f64,
         _bundle_path: &CStr,
         features: Option<&FeaturesList>,
     ) -> Option<Self>
@@ -30,16 +45,25 @@ impl Plugin for Midigate {
         let features = features?;
         let mut cached_map = CachedMap::try_from_features(features)?;
 
-        Some(Self {
+        let mut plugin = Self {
             control_port: AtomInputPort::new(&mut cached_map),
             in_port: AudioInputPort::new(),
+            null: Vec::new(),
             out_port: AudioOutputPort::new(),
 
             urid_map: cached_map,
 
             n_active_notes: 0,
-            program: 0,
-        })
+        };
+
+        // Allocate null space for one second of frames. This should be enough for most cases.
+        plugin.assure_null_len(rate as usize);
+
+        Some(plugin)
+    }
+
+    fn activate(&mut self) {
+        self.n_active_notes = 0;
     }
 
     unsafe fn connect_port(&mut self, port: u32, data: *mut ()) {
@@ -51,7 +75,68 @@ impl Plugin for Midigate {
         }
     }
 
-    fn run(&mut self, _n_samples: u32) {}
+    fn run(&mut self, n_samples: u32) {
+        // Assure that we have enough null space.
+        // Allocation of new space will occur rarely since one second of frames were already
+        // allocated at initialization.
+        // If we're in a real-time environment, the block sizes won't be longer than a second, and
+        // if we're not, than allocation time does not matter.
+        self.assure_null_len(n_samples as usize);
+
+        let mut offset: usize = 0;
+
+        let events_atom = unsafe { self.control_port.get_atom(&mut self.urid_map) }.unwrap();
+        let audio_input = unsafe { self.in_port.as_slice(n_samples) }.unwrap();
+        let null_input = &self.null.as_slice()[0..(n_samples as usize)];
+        let audio_output = unsafe { self.out_port.as_slice(n_samples) }.unwrap();
+
+        for (time_stamp, midi_event) in events_atom.iter(&mut self.urid_map).unwrap() {
+            // receiving note-ons and note-offs.
+            let midi_event = midi_event
+                .cast::<RawMidiMessage>(&mut self.urid_map)
+                .unwrap();
+            let midi_event = midi_event.interpret().unwrap();
+            match midi_event.unwrap_standard() {
+                StandardMidiMessage::NoteOn {
+                    channel: _,
+                    note: _,
+                    velocity: _,
+                } => {
+                    self.n_active_notes += 1;
+                }
+                StandardMidiMessage::NoteOff {
+                    channel: _,
+                    note: _,
+                    velocity: _,
+                } => {
+                    self.n_active_notes -= 1;
+                }
+                _ => (),
+            }
+
+            let time: usize = match time_stamp {
+                TimeStamp::Frames(frames) => frames as usize,
+                TimeStamp::Beats(_) => panic!("We can't handle beats!"),
+            };
+
+            let input = if self.n_active_notes > 0 {
+                &audio_input[offset..time]
+            } else {
+                &null_input[offset..time]
+            };
+            audio_output[offset..time].copy_from_slice(input);
+
+            offset += time;
+        }
+
+        let time = n_samples as usize;
+        let input = if self.n_active_notes > 0 {
+            &audio_input[offset..time]
+        } else {
+            &null_input[offset..time]
+        };
+        audio_output[offset..time].copy_from_slice(input);
+    }
 }
 
 lv2_main!(
