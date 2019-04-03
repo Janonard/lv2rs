91d460701a02db04a35203bea30e776f63f03ff7
Janonard <janonard@protonmail.com>
2019-04-03 13:14:15 +0200

    Removing the samples
    They have moved to their own repo.

--- samples/amp/run_plugin.sh
+++
@@ -1,5 +0,0 @@
-cargo build --release;
-cp target/release/libeg_amp_rs.so eg-amp-rs.lv2/libEgAmpRs.so;
-rm -r -f ~/.lv2/eg-amp-rs.lv2/;
-cp -r --parents eg-amp-rs.lv2 ~/.lv2/;
-jalv https://github.com/Janonard/eg-amp-rs;
